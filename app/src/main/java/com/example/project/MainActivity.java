b644445208ca1a1ccb103bf0b555d322de94b77e
Young9235 <inyoung9235@daum.net>
2019-05-10 19:52:14 +0900

    190510 project inyoung commit

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,8 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
     LinearLayout calendar, list, diary;
 
-    //kkkkkk
-
+    /** 하단 버튼 설정 **/
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -60,8 +61,33 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        final ImageView setButton = findViewById(R.id.setting);
+        /** 리스트 달력 **/
+        LayoutInflater inflater = getLayoutInflater();   //Layout XML 디자인 모습을 자바의 View 형태로 변환하는 것
 
+        inflater.inflate(R.layout.list_view, null);
+
+        ListView listView = findViewById(R.id.main_list_view);
+
+        AbookListVO vo1 =
+                new AbookListVO("1", "60,000");
+        AbookListVO vo2 =
+                new AbookListVO("2", "20,000");
+        AbookListVO vo3 =
+                new AbookListVO("3", "18,000");
+        AbookListVO vo4 =
+                new AbookListVO("4", "40,000");
+        AbookListVO
+                vo5 = new AbookListVO("5", "80,000");
+
+        AbookListVO data[] = {
+                vo1, vo2, vo3, vo4, vo5
+        };
+
+        CustomAdapter adapter = new CustomAdapter(MainActivity.this, R.layout.list_view, data);
+        listView.setAdapter(adapter);
+
+        /** 환경설정 버튼 **/
+        final ImageView setButton = findViewById(R.id.setting);
         setButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,4 +96,57 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+}
+
+/** 리스트형 달력 데이터 **/
+class AbookListVO {
+    String listDay;
+    String listMoney;
+    AbookListVO(String listDay, String listMoney) {
+        this.listDay = listDay;
+        this.listMoney = listMoney;
+    }
+}
+
+/** 리스트형 달력 어뎁터 **/
+class CustomAdapter extends BaseAdapter {
+    //생성자 포인터의 개념과 비슷? MainActivity를 가리키기 위함
+    MainActivity ma;
+    int layout;
+    AbookListVO[] data;
+
+    CustomAdapter(MainActivity ma, int view, AbookListVO[] data){
+        this.ma = ma;
+        this.layout = view;
+        this.data = data;
+    }
+
+    @Override
+    public int getCount() {
+        return data.length;               // 5개의 view의 모습이 list항목의 하나로 나온다.
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = ma.getLayoutInflater();
+        View view = inflater.inflate(layout, null);
+        TextView day = view.findViewById(R.id.list_day);
+        TextView money = view.findViewById(R.id.list_money);
+
+        day.setText(data[position].listDay);
+        money.setText(data[position].listMoney);
+
+        return view;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
 }
